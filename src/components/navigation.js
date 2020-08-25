d5ee5aadda1ab0fce4fe47bdb02480de5395a682
Daniel Fishman <fishfry62@gmail.com>
2020-08-25 10:29:52 -0700

    header and nav styling adjustments

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,31 +1,36 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import styles from './navigation.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRocket } from '@fortawesome/free-solid-svg-icons';
 
 export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/red/">Red</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blue/">Blue</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/green/">Green</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/pink/">Pink</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/purple/">Purple</Link>
-      </li>
-    </ul>
-  </nav>
+  <header className={styles.header}>
+    <FontAwesomeIcon className={styles.logo} icon={faRocket} size="2x"/>
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
+        <li className={styles.navItem}>
+          <Link to="/">Home</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/blog/">Blog</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/red/">Red</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/blue/">Blue</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/green/">Green</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/pink/">Pink</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/purple/">Purple</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 )
